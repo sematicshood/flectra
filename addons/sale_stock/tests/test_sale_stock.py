25845a690f38ef2a43c8c6b06fdf7aec247b5802
Yusuf Kapasi <yusuf@techreceptives.com>
2019-03-20 17:42:10 +0530

    [FIX] Resolve Boolean and None Error in sale_stock test case

--- addons/sale_stock/tests/test_sale_stock.py
+++ addons/sale_stock/tests/test_sale_stock.py
@@ -69,6 +69,7 @@ class TestSaleStock(TestSale):
         and whatever other model there is in stock with "invoice on order" products
         """
         # let's cheat and put all our products to "invoice on order"
+
         self.so = self.env['sale.order'].create({
             'partner_id': self.partner.id,
             'partner_invoice_id': self.partner.id,
